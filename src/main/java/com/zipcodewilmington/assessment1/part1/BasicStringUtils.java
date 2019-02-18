fe604ed6d3fa676f4d162fb0dbac05237ba66f8c
David Tamulavage <davidtamulavage@zipcoders-MacBook-Pro.local>
2019-02-18 09:52:15 -0500

    completed BasicStringUtils

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -9,7 +9,42 @@ public class BasicStringUtils {
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-        return null;
+
+        String temp;
+        StringBuilder sb = new StringBuilder();
+
+        temp =str.substring(0,1);
+
+        sb.append(temp.toUpperCase());
+        sb.append(str.substring(1));
+
+        return sb.toString();
+
+
+ // Actual Camel case Code
+        /*
+        String[] splitString = str.split(" ");
+        StringBuilder sb = new StringBuilder();
+        String temp = "";
+
+        for(String ele: splitString){
+            temp = "" + ele.charAt(0);  // get first char - convert back to string
+            temp = temp.toUpperCase(); // convert first character to upper
+            // appened first char to rest of string plus space
+            sb.append(temp);
+            for(int i=1; i<ele.length();i++){
+                sb.append(ele.charAt(i));
+            }
+
+            sb.append(" ");
+
+        }
+
+        sb.deleteCharAt(sb.length()-1); // Remove last space
+        sb.toString();
+
+        return sb.toString();
+        */
     }
 
     /**
@@ -17,7 +52,12 @@ public class BasicStringUtils {
      * @return string with identical contents, in the reverse order
      */
     public static String reverse(String str) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append(str);
+        sb.reverse();
+
+
+        return sb.toString();
     }
 
     /**
@@ -25,7 +65,9 @@ public class BasicStringUtils {
      * @return string with identical contents, in reverse order, with first character capitalized
      */
     public static String reverseThenCamelCase(String str) {
-        return null;
+        str = reverse(str);
+        str = camelCase(str);
+        return str;
     }
 
 
@@ -34,7 +76,8 @@ public class BasicStringUtils {
      * @return string with identical contents excluding first and last character
      */
     public static String removeFirstAndLastCharacter(String str) {
-        return null;
+
+        return str.substring(1,(str.length()-1));
     }
 
     /**
@@ -42,6 +85,28 @@ public class BasicStringUtils {
      * @return string with identical characters, each with opposite casing
      */
     public static String invertCasing(String str) {
-        return null;
+
+        StringBuilder sb = new StringBuilder();
+        int len = str.length();
+        char checkCase ;
+        String temp = "";
+
+        for(int i = 0; i<len; i++){
+            checkCase = str.charAt(i);
+            if(checkCase >= 'a' && checkCase <= 'z'){
+                temp=""+checkCase;
+                temp = temp.toUpperCase();
+                sb.append(temp);
+            }
+            else {
+                temp=""+checkCase;
+                temp = temp.toLowerCase();
+                sb.append(temp);
+            }
+
+
+        }
+
+        return sb.toString();
     }
 }
