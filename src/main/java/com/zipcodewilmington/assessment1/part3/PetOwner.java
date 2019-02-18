476e34a64462dff1b48771790da1d48c2db171e3
David Tamulavage <davidtamulavage@zipcoders-MacBook-Pro.local>
2019-02-18 11:48:57 -0500

    jumper part and sections of animal

--- src/main/java/com/zipcodewilmington/assessment1/part3/PetOwner.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/PetOwner.java
@@ -1,14 +1,21 @@
 package com.zipcodewilmington.assessment1.part3;
 
+import java.util.ArrayList;
+
 /**
  * Created by leon on 2/16/18.
  */
 public class PetOwner {
+    String name ;
+   public ArrayList<Pet> myPets  = new ArrayList<>();
+
     /**
      * @param name name of the owner of the Pet
      * @param pets array of Pet object
      */
     public PetOwner(String name, Pet... pets) {
+         this.name = name;
+       //  this.myPets = pets;
     }
 
     /**
--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -5,7 +5,30 @@ public class Jumper {
     /*
      * Complete the function below.
      */
-    public int jumps(int k, int j) {
-        return -1;
+    public int jumps(int k, int j){
+
+        int countOfJumps = 0;
+        int kJumps = 0;
+        int step1Jumps = 0;
+
+        // if 1 unit > max of jumps = set ret val to k
+        if ( j > k)
+        {
+            countOfJumps = k;
+        }
+        else if (k%j == 0)  // check for equal counts
+        {
+            countOfJumps = k/j;
+        }
+        else {
+            step1Jumps = k%j;
+            kJumps = k/j;
+            countOfJumps = step1Jumps+kJumps;
+        }
+
+
+
+
+    return countOfJumps;
     }
 }
